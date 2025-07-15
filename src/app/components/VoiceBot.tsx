9a616428b26aa3bc296b02877239be5cf92e99b1
ananyd36 <ananyd36@gmail.com>
2025-07-15 11:41:08 +0530

    refactored code

--- src/app/components/VoiceBot.tsx
+++ src/app/components/VoiceBot.tsx
@@ -71,7 +71,6 @@ export const VoiceBot: React.FC = () => {
                     End Call
                   </button>
                 </div>
-                {/* Transcript removed as per user request */}
               </div>
             )}
           </div>
